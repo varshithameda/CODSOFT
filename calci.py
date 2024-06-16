03bc9ba1873ccbfcc70e49d8565864d78c81cd81
varshithameda <163495267+varshithameda@users.noreply.github.com>
2024-06-16 21:17:27 +0530

    Add files via upload

---
+++ calci.py
@@ -0,0 +1,44 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+
+print("Welcome to Simple Calculator")
+
+while True:
+    print("Choose operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print("Result:", add(num1, num2))
+        elif choice == '2':
+            print("Result:", subtract(num1, num2))
+        elif choice == '3':
+            print("Result:", multiply(num1, num2))
+        elif choice == '4':
+            print("Result:", divide(num1, num2))
+    elif choice == '5':
+        print("Exiting the calculator.")
+        break
+    else:
+        print("Invalid input")
